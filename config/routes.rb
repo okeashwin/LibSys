1f83b29218c0bc38e1e849b2a454f9bf5a6f9732
Ashwin Oke <okeashwin@gmail.com>
2015-09-14 18:23:45 -0400

    Generic route entry

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  match ':controller(/:action(/:id))', :via => [ :get, :post]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
