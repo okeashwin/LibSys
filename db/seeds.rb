95b0538376bd3b4b219026e81f93fb735b1dc2ba
SHIVANI SHARMA <shivanidev2015@gmail.com>
2015-09-16 19:43:39 -0400

    Manage members excluding checkout history

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,28 +7,29 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 #The preconfigured admin
-#
-#  User.create(name: 'superUser', email: 'superUser@libsys.com', password: 'Password12', isDeleted: FALSE,
-#               role: User::IS_ADMIN)
-#
-# (1...6).each do |i|
-# name = 'member'+i.to_s
-# email = 'member'+i.to_s+'@gmail.com'
-# password = 'member'+i.to_s+'pwd'
-# User.create(name: name, email: email, password: password, isDeleted: FALSE,
-#             role: User::IS_MEMBER)
-# end
-#
-#  (1...6).each do |i|
-#    isbn = '978-3-16-148410-0-'+(i*20).to_s
-#    name = 'Book00'+i.to_s
-#    authors= 'bookAuthor'+i.to_s+',bookAuthor(i+1.to_s)'
-#    description = 'description string for book'+i.to_s
-#    Book.create(isbn:isbn, name: name, authors:authors,  description: description, isDeleted: FALSE,
-#                status: 0)
-#  end
+ User.create(name: 'superUser', email: 'superUser@libsys.com', password: 'Password12', isDeleted: FALSE,
+              role: User::IS_ADMIN)
 
-Reservation.create(user_id: 23, book_id: 1, dateIssued: Time.now.getutc)
- Reservation.create(user_id: 23, book_id: 2, dateIssued: Time.now.getutc)
- Reservation.create(user_id: 24, book_id: 3, dateIssued: Time.now.getutc)
- Reservation.create(user_id: 23, book_id: 4, dateIssued: Time.now.getutc)
+#Default members
+(1...6).each do |i|
+name = 'member'+i.to_s
+email = 'member'+i.to_s+'@gmail.com'
+password = 'member'+i.to_s+'pwd'
+User.create(name: name, email: email, password: password, isDeleted: FALSE,
+            role: User::IS_MEMBER)
+end
+
+#Default books
+ (1...6).each do |i|
+   isbn = '978-3-16-148410-0-'+(i*20).to_s
+   name = 'Book00'+i.to_s
+   authors= 'bookAuthor'+i.to_s+',bookAuthor(i+1.to_s)'
+   description = 'description string for book'+i.to_s
+   Book.create(isbn:isbn, name: name, authors:authors,  description: description, isDeleted: FALSE,
+               status: 0)
+ end
+
+Reservation.create(user_id: 4, book_id: 1, dateIssued: Time.now.getutc)
+ Reservation.create(user_id: 2, book_id: 2, dateIssued: Time.now.getutc)
+ Reservation.create(user_id: 3, book_id: 3, dateIssued: Time.now.getutc)
+ Reservation.create(user_id: 4, book_id: 4, dateIssued: Time.now.getutc)
