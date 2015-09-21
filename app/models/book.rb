832ebc3ec22cf9737a1f0ed8c28c327d95f44de1
Ashwin Oke <okeashwin@gmail.com>
2015-09-21 05:30:11 -0400

    Model Testcases

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,13 @@
 class Book < ActiveRecord::Base
 
+  # validation check columns
   validates :isbn, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :authors, presence: true
+  validates :status, presence: true
+
+  # Relationships
   has_many :reservations
+
   enum status: [:available, :checkedOut]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,15 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   # validation check columns
   validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :password, presence: true
+  validates :role, presence: true
+
+  # Relationships
   has_many :reservations
+
+  # Role checker targets
   IS_ADMIN = 1<<0
   IS_MEMBER = 1<<1
-
 
 end
